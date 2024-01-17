9655263648037ffe05d443064ad05e7eb2ab4260
Kenneth J. McClelland Jr <kxm203@yahoo.com>
2024-01-17 07:19:06 -0500

    I don't remember seeing .join before, but it makes sense.

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    const words = tutorial.split(" ");
+    const titleCaseWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    const titleCaseArray = titleCaseWords.join(" ");
+
+    return titleCaseArray;
+  });
 }
+console.log(titleCased());
